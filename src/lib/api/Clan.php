3b553ea44199d138b5183c9b96ee1f2751761b38
ErgoZ <ergozru@gmail.com>
2015-02-01 00:07:11 +0300

    Add 2 Clan functions

---
+++ src/lib/api/Clan.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ErgoZ
+ * Date: 30.01.2015
+ * Time: 13:32
+ */
+
+namespace RiftBit\WoTAPI\lib\api;
+use RiftBit\WoTAPI\lib\tools\Request;
+
+class Clan {
+
+    public static function clan_list($application_id, $api_url, $language, $fields, $search, $limit, $order_by, $page_no)
+    {
+        $request_params = [
+            'application_id' => $application_id,
+            'language' => $language,
+            'fields' => $fields,
+            'search' => $search,
+            'limit'=> $limit,
+            'order_by'=> $order_by,
+            'page_no'=> $page_no
+        ];
+        return Request::send($api_url."wot/clan/list/", $request_params);
+    }
+
+
+    public static function clan_info($application_id, $api_url, $language, $fields, $access_token, $clan_id)
+    {
+        $request_params = [
+            'application_id' => $application_id,
+            'language' => $language,
+            'fields' => $fields,
+            'access_token' => $access_token,
+            'clan_id' => $clan_id
+        ];
+        return Request::send($api_url."wot/clan/info/", $request_params);
+    }
+}
