f74c45d5ff098ac688dd6144f61cec17c966d27d
Rolando Guedes <rolando.guedes@3gnt.net>
2018-01-05 10:47:47 +0000

    Add defaults values

--- src/Api.php
+++ src/Api.php
@@ -24,37 +24,37 @@ class Api {
         $this->_application_language = $language;
     }
 
-    public function accountList($search, $fields, $type, $limit)
+    public function accountList($search, $fields="", $type="", $limit="")
     {
         return lib\api\Account::acc_list($this->_application_id, $this->_base_wot_api_url, $this->_application_language, $fields, $type, $search, $limit);
     }
 
-    public function accountInfo($account_id, $fields, $access_token)
+    public function accountInfo($account_id, $fields="", $access_token="")
     {
         return lib\api\Account::acc_info($this->_application_id, $this->_base_wot_api_url, $this->_application_language, $fields, $access_token, $account_id);
     }
 
-    public function accountTanks($account_id, $tank_id, $fields, $access_token)
+    public function accountTanks($account_id, $tank_id="", $fields="", $access_token="")
     {
         return lib\api\Account::acc_tanks($this->_application_id, $this->_base_wot_api_url, $this->_application_language, $fields, $access_token, $account_id, $tank_id);
     }
 
-    public function accountAchievements($account_id, $fields, $access_token)
+    public function accountAchievements($account_id, $fields="", $access_token="")
     {
         return lib\api\Account::acc_achievements($this->_application_id, $this->_base_wot_api_url, $this->_application_language, $fields, $access_token, $account_id);
     }
 
-    public function clanList($search, $fields, $limit, $order_by, $page_no)
+    public function clanList($search, $fields="", $limit="", $order_by="", $page_no="")
     {
         return lib\api\Clan::clan_list($this->_application_id, $this->_base_wot_api_url, $this->_application_language, $fields, $search, $limit, $order_by, $page_no);
     }
 
-    public function clanInfo($clan_id, $fields, $access_token)
+    public function clanInfo($clan_id, $fields="", $access_token="")
     {
         return lib\api\Clan::clan_info($this->_application_id, $this->_base_wot_api_url, $this->_application_language, $fields, $access_token, $clan_id);
     }
 
-    public function tanksStats($account_id, $tank_id, $access_token="", $fields="", $extra="", $in_garage=""){
+    public function tanksStats($account_id, $tank_id="", $access_token="", $fields="", $extra="", $in_garage=""){
         return lib\api\Tanks::tanks_stats($this->_application_id, $this->_base_wot_api_url, $this->_application_language, $account_id, $tank_id, $access_token, $fields, $extra, $in_garage );
     }
 
